ef9a41b80cec3d68ff4a6370ff18092750db3c57
Hernan Robledo <rbldhernan@gmail.com>
2026-09-12 09:41:46 -0500

    Delete backend/vite.config.js

--- backend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
-
-// Vite necesita estos plugins para entender React y Tailwind
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
